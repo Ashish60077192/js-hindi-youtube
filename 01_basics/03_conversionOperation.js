b789dcd0c1b7b138c94128f7e05876ad399144b0
Ashish60077192 <125450734+Ashish60077192@users.noreply.github.com>
2023-10-05 13:47:10 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,10 +21,25 @@
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//console.log("2" + 1 + 1)
+//console.log(2+1+"1");
+
+console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
 
 
 
